23452c73e166be42b04ba529b86d8921c2ec1086
BenPeng <asd5530025@gmail.com>
2019-12-27 15:49:52 +0800

    1. UI update

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ const Tab = (props) => {
   )
 }
 
-const Tabs = () => {
+const Tabs = (props) => {
   return (
     <div className='tabs is-centered is-boxed'>
       <ul>
@@ -64,8 +64,8 @@ const Tabs = () => {
           <Tab
             tab={tab}
             key={tab.name}
-            activeTab={this.props.activeTab}
-            changeActiveTab={this.props.changeActiveTab}
+            activeTab={props.activeTab}
+            changeActiveTab={props.changeActiveTab}
           />
         )}
       </ul>
@@ -74,10 +74,100 @@ const Tabs = () => {
 }
 
 const Index = () => {
+  const [activeTab, setActiveTab] = useState('展廳停留時間')
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+  const [startDateStr, setStartDateStr] = useState('')
+  const [endDateStr, setEndDateStr] = useState('')
+  const [dateError, setDateError] = useState(false)
+
+  const handleStartDateChange = date => {
+    setStartDate(date)
+  }
+
+  const handleEndDateChange = date => {
+    setEndDate(date)
+  }
+
+  const changeActiveTab = (tab) => {
+    setActiveTab(tab)
+  }
+
+  const onSearch = () => {
+    const startTime = startDate.getTime()
+    const endTime = endDate.getTime()
+    if (endTime > startTime) {
+      setStartDateStr(moment(startDate).format('YYYY-MM-DD'))
+      setEndDateStr(moment(startDate).format('YYYY-MM-DD'))
+      setDateError(false)
+    } else {
+      setDateError(true)
+    }
+  }
+
   return (
-    <div>
-      <Button color='primary'>My Bulma button</Button>
-    </div>
+    <Container>
+      <Box>
+        <Content>
+          <Columns>
+            <Columns.Column size={3}>
+              <Level.Side align='left'>
+                <Level.Item>
+                  <strong>開始時間</strong>
+                </Level.Item>
+                <Level.Item>
+                  <Form.Field kind='addons'>
+                    <DatePicker
+                      selected={startDate}
+                      onChange={handleStartDateChange}
+                      className='input'
+                    />
+                  </Form.Field>
+                </Level.Item>
+              </Level.Side>
+            </Columns.Column>
+            <Columns.Column size={3}>
+              <Level.Side align='left'>
+                <Level.Item>
+                  <strong>開始時間</strong>
+                </Level.Item>
+                <Level.Item>
+                  <Form.Field kind='addons'>
+                    <DatePicker
+                      selected={endDate}
+                      onChange={handleEndDateChange}
+                      className='input'
+                    />
+                  </Form.Field>
+                </Level.Item>
+              </Level.Side>
+            </Columns.Column>
+            <Columns.Column size={2}>
+              <Button renderAs='span' color='primary' onClick={() => onSearch()}>確定</Button>
+              {dateError ? (
+                <Tag color='danger' style={{ marginLeft: 20 }}>範圍選擇錯誤</Tag>
+              ) : (
+                null
+              )}
+            </Columns.Column>
+          </Columns>
+        </Content>
+      </Box>
+      <Box>
+        <Content activeTab={{ id: 'tab1' }} fullwidth>
+          <Tabs
+            tabList={tabList}
+            activeTab={activeTab}
+            changeActiveTab={changeActiveTab}
+          />
+          {startDateStr && endDateStr ? (
+            <GAChart startDate={startDateStr} endDate={endDateStr} />
+          ) : (
+            <p>請選擇搜尋範圍</p>
+          )}
+        </Content>
+      </Box>
+    </Container>
   )
 }
 export default Index
